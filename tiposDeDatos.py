4243117687f347cd0ca7a3b81f76aa050274857a
josthoon <josthoonrivas@gmail.com>
2023-07-08 19:38:33 -0300

    2,1

---
+++ tiposDeDatos.py
@@ -0,0 +1,20 @@
+#tipos de datos
+palabra = 'hola mundo'
+#string pueden ser con comillas simples o comillas dobles,no importa
+
+entero = 20 # integer
+decimal = 3.14 # float
+complejo = 1j 
+
+print   ( palabra, entero, decimal, complejo)
+
+#lista 
+mi_list =[1,'dos',True] # con datos
+list = [] #vacia
+
+print   (mi_list,list )
+
+mi_list.append (16) #metodo append cambia la lista sin necesidad de crear una nueva lista
+
+print(mi_list)
+
