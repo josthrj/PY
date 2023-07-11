66dcb53e3bbf1a223c2c14490d784e5e926b272c
josthoon <josthoonrivas@gmail.com>
2023-07-10 21:26:18 -0300

    continucion del contenido enfocado en listas

--- tiposDeDatos.py
+++ tiposDeDatos.py
@@ -8,13 +8,74 @@ complejo = 1j
 
 print   ( palabra, entero, decimal, complejo)
 
-#lista 
+#lista: son un  tipo de variable que permite el almacenamiento de varios,a los que se accede a traves de un indice
+#el tamano de una lista es igual al la cantidad de elementos que ella contiene
+
+#creando una lista
 mi_list =[1,'dos',True] # con datos
 list = [] #vacia
 
 print   (mi_list,list )
 
+#agregando elementos a una lista
 mi_list.append (16) #metodo append cambia la lista sin necesidad de crear una nueva lista
-
+ 
 print(mi_list)
+
+#mi_list.clear()
+#borrar todos los elementos de mi list
+
+list2 = mi_list.copy()
+##crear copia del contenido de mi_list en un nuevo objeto llamado "list2
+
+mi_list.append  (26) #agrega elementos a la lista, pero noi actualiza los cambios automaticamente en el meetodo copy
+
+
+print(mi_list,list2)
+
+# contando elementos y calculando el valor de repeticion en lista 
+
+print(list,list2,mi_list.count(16)) #metodo count
+
+#len la funcion len cuenta la cantidad de elementos presente en una cadena de caracteres o lista, es importante recordar que este conteo comienza en el indice 0
+#ej 012345678 >>> indice
+#ej abcdefghi >>> contenido
+
+
+#metodo 1
+print(len(mi_list), len (list2))
+
+
+
+#se puede ejecutar el metodo de las dos maneras sin ningun problema, solo que el metodo 1 es mas limpio
+#metodo 2
+largoList2 = len(list2)
+largoMi_list = len(mi_list)
+
+# print(largoList2, largoMi_list)
+
+
+#accedidiendo a los elementos de las listas
+print(mi_list[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
